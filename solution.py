333bddf3614a7015e033dbc88d9f4658357eb159
kjxeez <82978703+kjxeez@users.noreply.github.com>
2023-04-12 15:47:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,7 @@ import numpy as np
 
 chat_id = 584664949 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: #выборки параметра F
+    alpha = 0.05
+    p_val = stats.anderson_ksamp([x, y])[2]
+    return p_val < alpha
